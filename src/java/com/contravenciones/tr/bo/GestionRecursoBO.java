e6a19ea2b7d7d67b1c7710a5fe6a6b9b306f1303
rcamacho95 <35116389+rcamacho95@users.noreply.github.com>
2018-01-12 14:36:02 -0500

    listar persona

---
+++ src/java/com/contravenciones/tr/bo/GestionRecursoBO.java
@@ -0,0 +1,19 @@
+/*
+ * Civitrans
+ * La Cívica Impresores S.A.S
+ * Copyright 2016.
+ */
+package com.contravenciones.tr.bo;
+
+import com.contravenciones.jsf.bean.BeanGestionRecurso;
+
+
+/**
+ *
+ * @author Roymer Camacho
+ */
+public interface GestionRecursoBO {
+
+   
+    
+}
